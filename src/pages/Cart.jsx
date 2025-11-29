ad756f4601f80bf39b493aa1ccbde94f73c48e1c
coffee-marios <mark_theo@yahoo.com>
2025-11-29 12:43:18 +0200

    Create a component that returns the order in a table and is a child of Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,47 @@
 import { useOutletContext } from "react-router-dom";
+import "../styles/cart.css";
+
+const Order_table = () => {
+  const { orders } = useOutletContext();
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {Object.keys(orders).map((key) => (
+            <tr key={orders[key]["id"]}>
+              <th>{orders[key]["id"]}</th>
+
+              <td>{orders[key]["title"]}</td>
+              <td>{orders[key]["quantity"]}</td>
+              <td>{orders[key]["price"]}</td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default function Cart() {
-  const { orders } = useOutletContext();
-  const { setOrders } = useOutletContext();
   return (
     <div className="cart">
       <h1>WELCOME Cart</h1>
-      {Object.keys(orders).map((key) => (
-        <div key={key}>
-          <p>
-            {orders[key]["title"]} <span>{orders[key]["quantity"]}</span>{" "}
-          </p>
-        </div>
-      ))}
+
+      <Order_table />
     </div>
   );
 }
